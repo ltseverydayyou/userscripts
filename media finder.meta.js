dee1dd45f4bff4cf1eeabaf56038ae9172c3d5ba
Vyperia <117316014+ltseverydayyou@users.noreply.github.com>
2026-01-15 11:55:37 +0200

    Create media finder.meta.js

---
+++ media finder.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Media Finder (stable players + top toast + safe)
+// @namespace    http://tampermonkey.net/
+// @version      0.8
+// @description  Finds mp3/mp4/m3u8/etc without breaking sites; stable player detection; top toast; opens list on click
+// @match        *://*/*
+// @run-at       document-start
+// @grant        GM_openInTab
+// @downloadURL  https://github.com/ltseverydayyou/userscripts/raw/main/media%20finder.user.js
+// @updateURL    https://github.com/ltseverydayyou/userscripts/raw/main/media%20finder.meta.js
+// ==/UserScript==
